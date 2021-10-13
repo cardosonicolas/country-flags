9b3e885bf2f0bef0dc2af64c8226d60973671f37
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2021-10-13 16:19:10 -0300

    move hooks to its own directory

--- country-flags/src/components/Layout.js
+++ country-flags/src/components/Layout.js
@@ -1,56 +1,12 @@
-import { useEffect, useState } from "react";
 import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 import Navbar from "./Navbar";
-
-const LIGHT = "light",
-  DARK = "dark";
-
-const themes = {
-  [DARK]: {
-    background: "hsl(207, 26%, 17%)",
-    elements: "hsl(209, 23%, 22%)",
-    text: "hsl(0, 0%, 100%)",
-    box_shadow: "0px 0px 8px 0px rgb(0 0 0 / 50%) ",
-  },
-
-  [LIGHT]: {
-    background: "hsl(0, 0%, 98%)",
-    elements: "hsl(0, 0%, 100%)",
-    text: "hsl(200, 15%, 8%)",
-    box_shadow: "0px 0px 10px 0px rgb(59 59 59 / 8%)",
-  },
-};
-
-function useDarkTheme() {
-  const [theme, setTheme] = useState(LIGHT);
-
-  const toggleTheme = () => {
-    if (theme === LIGHT) {
-      setTheme(DARK);
-      window.localStorage.setItem(DARK, DARK);
-    } else {
-      setTheme(LIGHT);
-      window.localStorage.setItem(DARK, LIGHT);
-    }
-  };
-
-  useEffect(() => {
-    const theme = window.localStorage.getItem(DARK);
-    if (theme !== null) {
-      setTheme(theme);
-    } else {
-      window.localStorage.setItem(DARK, LIGHT);
-    }
-  }, []);
-
-  return [theme, toggleTheme];
-}
+import { useDarkTheme } from "../hooks";
 
 function Layout({ children }) {
   const [theme, toggleTheme] = useDarkTheme();
 
   return (
-    <ThemeProvider theme={themes[theme]}>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Navbar onChangeTheme={toggleTheme} theme={theme} />
       <Content>{children}</Content>
---
+++ country-flags/src/hooks/index.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+const LIGHT = "light",
+  DARK = "dark";
+
+const themes = {
+  [DARK]: {
+    background: "hsl(207, 26%, 17%)",
+    elements: "hsl(209, 23%, 22%)",
+    text: "hsl(0, 0%, 100%)",
+    box_shadow: "0px 0px 8px 0px rgb(0 0 0 / 50%) ",
+  },
+
+  [LIGHT]: {
+    background: "hsl(0, 0%, 98%)",
+    elements: "hsl(0, 0%, 100%)",
+    text: "hsl(200, 15%, 8%)",
+    box_shadow: "0px 0px 10px 0px rgb(59 59 59 / 8%)",
+  },
+};
+
+export function useDarkTheme() {
+  const [theme, setTheme] = useState(LIGHT);
+
+  const toggleTheme = () => {
+    if (theme === LIGHT) {
+      setTheme(DARK);
+      window.localStorage.setItem(DARK, DARK);
+    } else {
+      setTheme(LIGHT);
+      window.localStorage.setItem(DARK, LIGHT);
+    }
+  };
+
+  useEffect(() => {
+    const theme = window.localStorage.getItem(DARK);
+    if (theme !== null) {
+      setTheme(theme);
+    } else {
+      window.localStorage.setItem(DARK, LIGHT);
+    }
+  }, []);
+
+  return [themes[theme], toggleTheme];
+}
